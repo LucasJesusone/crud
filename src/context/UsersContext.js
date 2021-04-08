deafbaff971571e7478caac4771f25693605fa3e
lucasjesusone <lucasjesusone@gmail.com>
2021-04-07 23:12:08 -0400

    Inserindo Context API

---
+++ src/context/UsersContext.js
@@ -0,0 +1,50 @@
+import React, { createContext, useReducer } from 'react'
+import users from '../data/users'
+
+const initialState = {users}
+const UsersContext = createContext({})
+
+
+const actions = {
+    CREATE_USER(state, action) {
+        const user = action.payload
+        user.id = Math.random()
+        return {
+            ...state, // estado atual
+            users: [...state.users, user],
+        }
+    },
+    UPDATE_USER(state, action) {
+        const updatedUser = action.payload 
+        return {
+            ...state,
+            users: state.users.map(u => u.id === updatedUser.id ? updatedUser : u)
+        }
+    },
+    DELETE_USER(state, action) {
+        const user = action.payload
+        return { 
+            ...state, 
+            users: state.users.filter(u => u.id !== user.id)
+        }
+    }
+}
+
+export const UsersProvider = props => {
+    
+        function reducer(state, action) {
+            const fn = actions[action.type]
+            return fn ? fn(state, action) : state
+        }
+
+
+    const [ state, dispatch ] = useReducer(reducer, initialState)
+
+    return (
+        <UsersContext.Provider value={{state, dispatch}}>
+            {props.children}
+        </UsersContext.Provider>
+    )
+} 
+
+export default UsersContext
